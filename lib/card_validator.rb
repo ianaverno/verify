1046fa11497e88998dadd6143c343f9fb5b16028
Vadim <vadim.entin@gmail.com>
2016-12-25 22:24:18 +0300

    Get the identify_provider tests to pass

--- lib/card_validator.rb
+++ lib/card_validator.rb
@@ -1,2 +1,35 @@
 class CardValidator
+
+  def self.identify_provider(input_number)
+    card_number = input_number.gsub(/\s+/, "")
+
+    if is_amex?(card_number)
+      return "AMEX"
+    elsif is_discover?(card_number)
+      return "DISCOVER"
+    elsif is_mastercard?(card_number)
+      return "MASTERCARD"
+    elsif is_visa?(card_number)
+      return "VISA"
+    else
+      return "UNKNOWN"
+    end
+  end
+
+  private
+    def self.is_amex?(number)
+      /^3[47]\d{13}$/ === number
+    end
+
+    def self.is_discover?(number)
+      /^6011\d{12}$/ === number
+    end
+
+    def self.is_mastercard?(number)
+      /^5[1-5]\d{14}$/ === number
+    end
+
+    def self.is_visa?(number)
+      /^4\d{15}$/ === number or /^4\d{12}$/ === number
+    end
 end
